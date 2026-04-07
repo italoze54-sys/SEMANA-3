abd0b758e1f760de1cdbea7320db0e0e36e93421
ItaloCenas <italoze54@gmail.com>
2026-04-06 20:16:19 -0500

    as

---
+++ objetos.py
@@ -0,0 +1,9 @@
+class Perro:
+    
+    def __init__(self, _nombre="Jacob"):
+        self.nombre=_nombre
+        self.edad=1
+        
+    def ladra(self):
+        print(f"Guau tengo {self.edad} años y me llamo {self.nombre}")
+    
